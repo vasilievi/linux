560c9abbe6002a6773b123addd0bb42dfeae20df
vasilievi <vasilievi@outlook.com>
2025-11-14 21:55:18 +0300

    init

---
+++ docusaurus.config.ts
@@ -0,0 +1,123 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'Linux',
+  tagline: 'Особенности настройки Linux',
+  favicon: 'img/favicon.ico',
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: 'https://vasilievi.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/linux/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'vasilievi', // Usually your GitHub org/user name.
+  projectName: 'linux', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'ru',
+    locales: ['ru'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/vasilievi/linux/tree/main/',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      title: 'Linux',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Курс подготовки',
+        },
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Примеры билетов',
+          items: [
+            {
+              label: 'Сборник задач',
+              href: 'https://v8.1c.ru/metod/books/106055.htm',
+            },
+          ],
+        }, 
+        {
+          title: 'Основа для решения задач',
+          items: [
+            {
+              label: 'Каркасная конфигурация',
+              href: 'https://static.1c.ru/rus/partners/training/files/new_carcass_8_3_25_1445.dt',
+            },
+          ],
+        },
+        {
+          title: 'Ознакомиться перед экзаменом',
+          items: [
+            {
+              label: 'Требования к экзамену',
+              href: 'https://static.1c.ru/rus/partners/training/files/ATT83PL.rtf',
+            },
+          ],
+        },
+      ],
+      copyright: `${new Date().getFullYear()} | ПрофКейс | pcs.ru`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
